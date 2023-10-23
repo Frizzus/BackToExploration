b526db4e1c2bfb77f3a60fb21d3ee4f01226b1c0
Frizzus <loiccanin.pro@gmail.com>
2023-10-23 20:35:32 +0200

    beginning of datapacj struct creation and assets organization

---
+++ main.py
@@ -0,0 +1,26 @@
+import os, sys, shutil, json
+from lib.dimension import CheckerBoard 
+
+# suppression du datapack précédent
+
+shutil.rmtree("bte")
+
+# création du template du datapack
+
+datapack_templating:list[str] = ["bte", "bte/data", "bte/minecraft", "bte/minecraft/dimension", "bte/data/worldgen", "bte/data/worldgen/biome", "bte/data/worldgen/configured_feature", "bte/data/worldgen/placed_feature"]
+
+for directory in datapack_templating:
+    os.mkdir(directory)
+
+mc_meta = open("bte/pack.mcmeta", "x")
+
+mc_meta.write(
+"""{
+    "pack":{
+        "pack_format":15,
+        "description":"Discover all the possibilities !"
+    }
+}""")
+
+debug_dim:CheckerBoard = CheckerBoard("3", ["minecraft:forest", "minecraft:meadow"])
+debug_dim.create_dim("bte/minecraft/dimension/overworld.json")
