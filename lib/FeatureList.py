b10df84c170dfac7b8900e40aaa7595595ef8f49
Frizzus <loiccanin.pro@gmail.com>
2023-10-27 13:01:53 +0200

    weighted_choice func created no test

--- lib/FeatureList.py
+++ lib/FeatureList.py
@@ -20,6 +20,7 @@ class FeatureList:
         if (cat_object["allowed_temperature_range"][1] >= allowed_temperature and allowed_temperature >= cat_object["allowed_temperature_range"][0]) and (cat_object["allowed_downfall_range"][1] >= allowed_downfall and allowed_downfall >= cat_object["allowed_downfall_range"][0]):
             nb_cat_iteration:int = 0
             random_nb = random.randint(1, 100)
+
             for percentage in cat_object["multiple_occurence_percentage"]:
                 if random_nb <= percentage:
                     nb_cat_iteration += 1
---
+++ lib/utilities.py
@@ -0,0 +1,19 @@
+import random
+
+def weighted_choice(values:list, weights:list[int], over:int) -> any:
+    weights_sum:int = 0
+    weights_ranges:list[int] = []
+
+    for nb in weights:
+        weights_sum += nb
+        weights_ranges.append(weights_sum)
+
+    if weights_sum != over:
+        raise ValueError("The values in |weights| do not add up to |over| : "+str(over))
+
+    random_value:int = random.randint(1, over)
+
+    for index in range(len(weights_ranges)):
+        if random_value <= weights_ranges[index]:
+            return values[index]
+    
